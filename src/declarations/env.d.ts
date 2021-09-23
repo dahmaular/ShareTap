67a5fecfc31544077cb406434264ccdb4171cc7b
adeyemosamuel <adeyemosamuel@gmail.com>
2021-09-23 13:03:38 +0100

    storybook installation

--- src/declarations/env.d.ts
+++ src/declarations/env.d.ts
@@ -3,5 +3,6 @@ declare module 'react-native-dotenv' {
     export const APPSYNC_APIKEY: string;
     export const APPSYNC_REGION: string;
     export const APPSYNC_APPID: string;
+    export const LOAD_STORYBOOK: boolean;
     export const ENV: 'dev' | 'prod';
   } 
---
+++ storybook/rn-addons.js
@@ -0,0 +1,5 @@
+import '@storybook/addon-ondevice-actions/register';
+import '@storybook/addon-ondevice-knobs/register';
+import '@storybook/addon-actions';
+import '@storybook/addon-knobs';
+import '@storybook/addon-links';
