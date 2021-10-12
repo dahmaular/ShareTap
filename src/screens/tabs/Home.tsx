d547ded763c1e46703aa370b6cc5af713e912d42
adeyemosamuel <adeyemosamuel@gmail.com>
2021-10-12 09:23:55 +0100

    alert component

--- src/screens/tabs/Home.tsx
+++ src/screens/tabs/Home.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
 import Header from '../../components/Header';
 import {
   AuthenticatedRoutesParamsList,
@@ -10,6 +17,8 @@ import {Badge} from 'react-native-paper';
 import Menu from '../../assets/svg/menu.svg';
 import Notification from '../../assets/svg/ion-ios-notifications.svg';
 import {StackNavigationProp} from '@react-navigation/stack';
+import {BACKGROUND_COLOR} from '../../core/color';
+import Message from '../../components/Message';
 
 type Props = {
   navigation: CompositeNavigationProp<
@@ -18,7 +27,11 @@ type Props = {
   >;
 };
 
+const {width} = Dimensions.get('screen');
+
 const Home = ({navigation}: Props) => {
+  const [message, setMessage] = useState('');
+
   return (
     <View style={{flex: 1}}>
       <Header
@@ -38,6 +51,50 @@ const Home = ({navigation}: Props) => {
         }
         rightOnPress={() => <></>}
       />
+
+      <View style={styles.container}>
+        <ScrollView
+          contentContainerStyle={{flexGrow: 1}}
+          alwaysBounceVertical={false}
+          showsVerticalScrollIndicator={false}
+          keyboardShouldPersistTaps="handled">
+          <View style={styles.homeContainer}>
+            {message != '' && (
+              <View style={styles.toastView}>
+                <Message
+                  message={message}
+                  onHide={() => {
+                    setMessage('');
+                  }}
+                />
+              </View>
+            )}
+            <View style={styles.organize}>
+              <View>
+                <Text style={styles.organizeText}>Organize cards shared</Text>
+                <Text style={styles.organizeText}>
+                  with you in your Rolodex
+                </Text>
+              </View>
+
+              <TouchableOpacity
+                style={styles.viewButton}
+                onPress={() =>
+                  setMessage('Upgrade to premium to unlock full access.')
+                }>
+                <Text style={styles.viewButtonText}>View Rodolex</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.yourCards}>
+              <Text style={styles.yourCardsText}>Your Cards (4)</Text>
+              <Text style={styles.viewAll}>View all</Text>
+            </View>
+
+            
+          </View>
+        </ScrollView>
+      </View>
     </View>
   );
 };
@@ -51,5 +108,83 @@ const styles = StyleSheet.create({
     right: 1,
     backgroundColor: '#FF4E00',
     color: '#FFFFFF',
+  },
+
+  container: {
+    flex: 1,
+  },
+
+  homeContainer: {
+    backgroundColor: BACKGROUND_COLOR,
+    height: '100%',
+    width: width,
+    alignItems: 'center',
+    paddingHorizontal: 15,
+  },
+
+  organize: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: 56,
+  },
+
+  viewButton: {
+    width: 132,
+    height: 48,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 2,
+    borderWidth: 1,
+    borderColor: '#316F8A',
+  },
+
+  viewButtonText: {
+    fontFamily: 'Poppins',
+    fontSize: 12,
+    fontStyle: 'normal',
+    fontWeight: '500',
+    color: '#316F8A',
+  },
+
+  organizeText: {
+    fontFamily: 'Poppins',
+    fontSize: 14,
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    color: 'rgba(51, 51, 51, 0.55)',
+  },
+
+  toastView: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+  },
+
+  yourCards: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: 52,
+  },
+
+  yourCardsText: {
+    fontFamily: 'Poppins',
+    fontSize: 14,
+    fontStyle: 'normal',
+    fontWeight: '500',
+    color: '#333333',
+  },
+
+  viewAll: {
+    fontFamily: 'Poppins',
+    fontSize: 14,
+    fontStyle: 'normal',
+    fontWeight: '500',
+    color: '#316F8A',
   },
 });
