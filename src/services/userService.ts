822de1281d59e146731c22a508f6668a8e022757
Chris <christianotu.d@gmail.com>
2021-10-22 14:11:39 +0100

    Updated with listUserCards service

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,5 +1,13 @@
-import {API, Auth, withSSRContext} from 'aws-amplify';
+import {API, Auth} from 'aws-amplify';
 import {GRAPHQL_AUTH_MODE} from '@aws-amplify/api';
+
+import {listUserCards} from '../graphql/queries';
+import {ListUserCardsQuery} from '../types/apiTypes';
+import {ExtractType} from '../types/extractApiTypes';
+
+export type ListUserCardsResponse = ExtractType<ListUserCardsQuery>;
+
+export type TodoType = ExtractType<ListUserCardsResponse>;
 
 export const getUserIdService = async () => {
   try {
@@ -7,7 +15,22 @@ export const getUserIdService = async () => {
 
     const userId = `USR-${user?.attributes.sub}`;
 
-    return {userId};
+    return userId;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const listUserCardsService = async (userId: string) => {
+  try {
+    const {data} = (await API.graphql({
+      query: listUserCards,
+      variables: {userId},
+      authMode: GRAPHQL_AUTH_MODE.API_KEY,
+    })) as {
+      data: ListUserCardsResponse;
+    };
+    return {data: data.listUserCards.cards};
   } catch (error) {
     throw error;
   }
