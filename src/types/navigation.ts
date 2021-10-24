56359a45c709eb832d63a2dfd981ee67678996fd
adeyemosamuel <adeyemosamuel@gmail.com>
2021-10-24 08:46:47 +0100

    navigation types

--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -1,24 +1,8 @@
 import {NavigatorScreenParams} from '@react-navigation/native';
 
-export interface ItemProps {
-  id: number;
-  name: string;
-  phone: string;
-  email: string;
-  profession: string;
-}
-
 export interface ConnectProps {
   id: number;
   name: string;
-}
-
-export interface CardProps {
-  item: ItemProps;
-  index: number;
-  boxWidth: number;
-  halfBoxDistance: number;
-  pan: any;
 }
 
 export type TabNavigatorParamsList = {
