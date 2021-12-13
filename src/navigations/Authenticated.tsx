1b315464af0ae52f6ccd61c096a6f649f6865300
adedamola <dahmaular@gmail.com>
2021-12-13 15:41:09 +0100

    started create card feature

--- src/navigations/Authenticated.tsx
+++ src/navigations/Authenticated.tsx
@@ -6,6 +6,7 @@ import DrawerContent from './DrawerContent';
 import {AuthenticatedRoutesParamsList} from '../types';
 import Search from '../screens/private/Search';
 import Rolodex from '../screens/private/Rolodex';
+import CreateCard from '../screens/private/Create_Card';
 
 const Drawer = createDrawerNavigator<AuthenticatedRoutesParamsList>();
 
@@ -28,6 +29,12 @@ export default function AuthenticatedRoutes() {
       <Drawer.Screen
         name="Rolodex"
         component={Rolodex}
+        options={{headerShown: false}}
+      />
+
+      <Drawer.Screen
+        name="CreateCard"
+        component={CreateCard}
         options={{headerShown: false}}
       />
     </Drawer.Navigator>
