a08751ca31df72734a392a92c2a30eecfaedb0c8
adedamola <dahmaular@gmail.com>
2022-03-18 10:32:39 +0100

    Added user data on Drawer menu

--- src/screens/tabs/Home.tsx
+++ src/screens/tabs/Home.tsx
@@ -63,7 +63,7 @@ const Home = ({navigation}: Props) => {
   const halfBoxDistance = boxDistance / 2;
   const pan = useRef(new Animated.ValueXY()).current;
   const [userId, setUserId] = useState('');
-  // console.log('User data @home', user?.cards?.listUserCards?.cards);
+  console.log('User data @home', user);
   const _onNotificationPressed = () => {
     setModal(true);
   };
@@ -73,7 +73,7 @@ const Home = ({navigation}: Props) => {
       .then(id => {
         // console.log('Id is here', id);
         setUserId(id);
-        console.log('app sync ', APPSYNC_APIKEY.toString());
+        // console.log('app sync ', APPSYNC_APIKEY.toString());
       })
       .catch(e => console.log(e));
   }, []);
