2a463457dd85f9fa6388944dd760f50dc4999172
adeyemosamuel <adeyemosamuel@gmail.com>
2022-03-21 10:54:10 +0100

    Schedule Services

--- src/services/rolodexService.ts
+++ src/services/rolodexService.ts
@@ -3,16 +3,34 @@ import {API, Auth} from 'aws-amplify';
 import {GRAPHQL_AUTH_MODE} from '@aws-amplify/api';
 
 import {ExtractType} from '../types/extractApiTypes';
-import {listBusinessCategories, listReceivedCards} from '../graphql/queries';
 import {
+  listBusinessCategories,
+  listReceivedCards,
+  listScheduledMessages,
+} from '../graphql/queries';
+import {
+  CreateScheduleMessageMutation,
   ListBusinessCategoriesQuery,
   ListReceivedCardsQuery,
+  ListScheduledMessagesQuery,
+  ReminderInput,
+  ScheduleMessageInput,
+  SetReminderMutation,
 } from '../types/apiTypes';
+import {createScheduleMessage, setReminder} from '../graphql/mutations';
 
 export type ListBusinessCategoriesResponse =
   ExtractType<ListBusinessCategoriesQuery>;
 
 export type ListReceivedCardsResponse = ExtractType<ListReceivedCardsQuery>;
+
+export type ScheduleMessageResponse =
+  ExtractType<CreateScheduleMessageMutation>;
+
+export type SetReminderResponse = ExtractType<SetReminderMutation>;
+
+export type ListScheduledMessagesResponse =
+  ExtractType<ListScheduledMessagesQuery>;
 
 export const listCategoriesService = async () => {
   try {
@@ -38,6 +56,53 @@ export const listReceivedCardsService = async (userId: string) => {
       data: ListReceivedCardsResponse;
     };
     return {data: data.listReceivedCards};
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const scheduleMessageService = async (
+  scheduleMessagePayload: ScheduleMessageInput,
+) => {
+  try {
+    const {data} = (await API.graphql({
+      query: createScheduleMessage,
+      variables: {scheduleMessagePayload: scheduleMessagePayload},
+      authMode: GRAPHQL_AUTH_MODE.API_KEY,
+    })) as {
+      data: ScheduleMessageResponse;
+    };
+    return {data: data.createScheduleMessage};
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const setReminderService = async (reminderPayload: ReminderInput) => {
+  try {
+    const {data} = (await API.graphql({
+      query: setReminder,
+      variables: {reminderPayload: reminderPayload},
+      authMode: GRAPHQL_AUTH_MODE.API_KEY,
+    })) as {
+      data: SetReminderResponse;
+    };
+    return {data: data.setReminder};
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const ListScheduledMessagesService = async (userId: string) => {
+  try {
+    const {data} = (await API.graphql({
+      query: listScheduledMessages,
+      variables: {userId},
+      authMode: GRAPHQL_AUTH_MODE.API_KEY,
+    })) as {
+      data: ListScheduledMessagesResponse;
+    };
+    return {data: data.listScheduledMessages};
   } catch (error) {
     throw error;
   }
